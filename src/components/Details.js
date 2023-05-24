22c38bf034eb76c5c4ef4bfe657cfe39a394cace
AllTwinkleStars <KeepGrowinga@gmail.com>
2023-05-24 11:16:53 -0300

    Product Detail Style Change
    Co-authored-by: multikitty <multikitty422@gmail.com>

--- src/components/Details.js
+++ src/components/Details.js
@@ -26,8 +26,8 @@ export default class Details extends Component {
                             {/*product text*/}
                                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
                                     <h2>model:{title}</h2>
-                                    <h4 className="text-title text-capitalize text-muted mt-3 mb-2">
-                                        made by: <span className="text-capitalize">{company}</span>
+                                    <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                                        made by: <span className="text-uppercase">{company}</span>
                                     </h4>
                                     <h4 className="text-blue">
                                         <strong>
